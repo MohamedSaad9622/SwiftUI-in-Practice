5724386f67d864bc2730a86b680ca10df0238f28
mohamed.khalf <mohamed.saad.khalfallah@gmail.com>
2025-05-08 16:25:11 +0300

    Initial Commit

---
+++ SpotifyBySWiftUI/SpotifyBySWiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyBySWiftUIApp.swift
+//  SpotifyBySWiftUI
+//
+//  Created by Mohamed Saad on 08/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyBySWiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
